8c61b2b12ba7d8f0e44cd35e109478effbe9ae50
Zakhar Gogolev <zargog74@gmail.com>
2021-06-04 12:40:07 +0500

    2

--- Project1/Project1/Task1.c
+++ Project1/Project1/Task1.c
@@ -6,12 +6,18 @@
 
 #include"Task1.h"
 
-int** new(int size_x, int size_y)
+struct Size_matrix
 {
-	int** massive = (int**)malloc(size_x * sizeof(int*));
-	for (int i = 0; i < size_x; i++)
+	int string;
+	int columns;
+} size;
+
+int** new(int string, int columns)
+{
+	int** massive = (int**)malloc(string * sizeof(int*));
+	for (int i = 0; i < string; i++)
 	{
-		massive[i] = (int*)malloc(size_y * sizeof(int));
+		massive[i] = (int*)malloc(columns * sizeof(int));
 	}
 
 	return(massive);
@@ -19,7 +25,38 @@ int** new(int size_x, int size_y)
 
 int** delete(int** matrix)
 {
+	for (int i = 0; i < size.string; i++)
+	{
+		free(matrix[i]);
+	}
+	free(matrix);
 
+	return(matrix);
+}
+
+int** cin(int** matrix)
+{
+	for (int i = 0; i < size.string; i++)
+	{
+		for (int j = 0; j < size.columns; j++)
+		{
+			scanf_s("%d", &matrix[i][j]);
+		}
+	}
+
+	return(matrix);
+}
+
+int cout(int** matrix)
+{
+	for (int i = 0; i < size.string; i++)
+	{
+		for (int j = 0; j < size.columns; j++)
+		{
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 
@@ -29,10 +66,27 @@ void Matrix()
 	printf("Task 1\n");
 	setlocale(0, "ru");
 
-	int** matrix = new(10, 10);
+	struct Size_matrix* count = (struct Size_matrix*)malloc(2 * sizeof(struct Size_matrix));
 
-	matrix[0][0] = 10;
-	printf("%d\n", matrix[0][0]);
+	printf("размер матрицы 1\n:");
+	scanf_s("%d", count[0].string);
 
-	
+
+	scanf_s("%d", count[0].string);
+	scanf_s("%d", count[0].columns);
+	scanf_s("%d", count[1].string);
+	scanf_s("%d", count[1].columns);
+
+	int** matrix_1 = new(count[0].string, count[0].columns);
+	int** matrix_2 = new(count[1].string, count[1].columns);
+
+	cin(matrix_1);
+	printf("\n");
+	cin(matrix_2);
+	printf("\n");
+
+	cout(matrix_1);
+	printf("\n");
+	cout(matrix_2);
+	printf("\n");
 }
